d022dd1699c70d9c625f89370678865ae204ce74
liuzhaoting <liuzhaoting@100tal.com>
2019-08-06 15:37:50 +0800

    new

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,20 @@ import * as React from 'react'
  interface IProps{
 	name?:string;
 }
+ 
 interface IState{
 	count:number
 }
 export default class App extends React.Component<IProps,IState>{
+	public static defaultProps:IProps = {
+		name:'lzt'
+	};
 	public state = {
 		count:100
+	}
+
+	constructor(props:IProps){
+		super(props)
 	}
 	public render(){
 		return(
@@ -15,4 +23,5 @@ export default class App extends React.Component<IProps,IState>{
 		)
 	}
 }
-
+ 
+ 
